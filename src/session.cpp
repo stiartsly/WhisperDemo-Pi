8d6cfda4ff3769b5245485db241aa2481ac30485
stiartsly <stiartsly@gmail.com>
2018-04-19 14:03:33 +0800

    bugfix for whisper_session_start() calltime

--- src/session.cpp
+++ src/session.cpp
@@ -4,6 +4,46 @@
 #include "vlog.h"
 #include "session.h"
 
+class state2str {
+public:
+    state2str(WhisperStreamState state) {
+        mState = state;
+    }
+
+    operator const char*() const {
+        const char *_str;
+
+        switch(mState) {
+        case WhisperStreamState_initialized:
+            _str = "Initialized";
+            break;
+        case WhisperStreamState_transport_ready:
+            _str = "Transport ready";
+            break;
+        case WhisperStreamState_connecting:
+            _str = "Connecting...";
+            break;
+        case WhisperStreamState_connected:
+            _str = "Connected";
+            break;
+        case WhisperStreamState_deactivated:
+            _str = "Deactivated";
+            break;
+        case WhisperStreamState_closed:
+            _str = "Closed";
+            break;
+        case WhisperStreamState_failed:
+        default:
+            _str = "Failed";
+            break;
+        }
+        return _str;
+    }
+
+private:
+    WhisperStreamState mState;
+};
+
 CSession::~CSession()
 {
     close();
@@ -16,6 +56,8 @@ void onStreamStateChanged(WhisperSession *session, int stream,
     CSession *sess = static_cast<CSession*>(context);
     int rc;
 
+    vlogD("Stream %s state changed to %s", state2str(state));
+
     if (state == WhisperStreamState_initialized) {
         rc = whisper_session_reply_request(session, 0, NULL);
         if (rc < 0) {
@@ -25,11 +67,10 @@ void onStreamStateChanged(WhisperSession *session, int stream,
             sess->close();
             return;
         }
-
+    } else if (state == WhisperStreamState_transport_ready) {
         const char *sdp = sess->getSdp();
         whisper_session_start(session, sdp, strlen(sdp));
         vlogI("Session with %s started", sess->getTo());
-
     } else if (state == WhisperStreamState_connected) {
         vlogI("Stream with %s connected", sess->getTo());
         sess->stream(stream);
@@ -38,7 +79,6 @@ void onStreamStateChanged(WhisperSession *session, int stream,
         sess->close();
     } else if (state == WhisperStreamState_closed ||
         state == WhisperStreamState_failed) {
-
         vlogI("Session with %s closed by remote", sess->getTo());
         sess->close();
     }
@@ -97,9 +137,7 @@ void CSession::write(const uint8_t *data, size_t len)
     ssize_t rc;
 
     rc = whisper_stream_write(mSession, mStream, data, len);
-    if (rc < 0) {
+    if (rc < 0)
         vlogE("Write data to stream %d error: 0x%x", mStream,
             whisper_get_error());
-        return;
-    }
 }
